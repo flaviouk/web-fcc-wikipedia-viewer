5cafb6eaf92baba65b2a2706ff38e2a476d3cd6c
flaviocarvalho <flavio046@hotmail.com>
2016-05-23 20:14:24 -0500

    code cleanup

--- js/app.js
+++ js/app.js
@@ -1,11 +1,42 @@
 $(document).ready(init);
 
 function init(){
+	function ajaxSuccess(data){
+		for(var key in data){
+			var title = data[key].title;
+			var description = data[key].extract;
+			var image = data[key].thumbnail.source;
+			var link = 'https://en.wikipedia.org/?curid=' + data[key].pageid;
+
+			var html = 
+				'<div class="card cyan darken-4">' +
+				'<div class="row">' +
+				'<div class="col s3">' +
+				'<img id="image" src="' + image + '">' +
+				'</div>' +
+				'<div class="col s9">' +
+				'<div class="card-content white-text">' +
+				'<span class="card-title">' + title + '</span>' +
+				'<p>' + description + '</p>' +
+				'</div>' +
+				'<div class="card-action">' +
+				'<a href="' + link + '">Learn more</a>' +
+				'</div>' +
+				'</div>' +
+				'</div>' +
+				'</div>';
+
+			$('.item-content').append(html);
+			}
+		}
+	
 	function ajax(){
 			var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
 			var query = $('#search-btn').val();
 			var cb = '&callback=JSON_CALLBACK';
 			var page = 'https://en.wikipedia.org/?curid=';
+
+			
 
 			$.ajax({
 				url: api + query + cb,
@@ -14,36 +45,8 @@ function init(){
 				success: function(data){
 					$('.item-content').empty();
 					var dataResults = data.query.pages;
-					
-					console.log(dataResults);
 
-					for(var key in dataResults){
-						var dataTitle = dataResults[key].title;
-						var dataDescription = dataResults[key].extract;
-						var dataImage = dataResults[key].thumbnail.source;
-						var dataLink = 'https://en.wikipedia.org/?curid=' + dataResults[key].pageid;
-
-						var html = 
-							'<div class="card cyan darken-4">' +
-							'<div class="row">' +
-							'<div class="col s3">' +
-							'<img id="image" src="' + dataImage + '">' +
-							'</div>' +
-							'<div class="col s9">' +
-							'<div class="card-content white-text">' +
-							'<span class="card-title">' + dataTitle + '</span>' +
-							'<p>' + dataDescription + '</p>' +
-							'</div>' +
-							'<div class="card-action">' +
-							'<a href="' + dataLink + '">Learn more</a>' +
-							'</div>' +
-							'</div>' +
-							'</div>' +
-							'</div>';
-
-						$('.item-content').append(html);
-					}
-
+					ajaxSuccess(dataResults);
 				},
 				error: function(){
 					alert('Error');
@@ -53,7 +56,7 @@ function init(){
 
 	$('#search-btn').keypress(function(event) {
 		if(event.which == 13){
-			$('.item').empty();
+			$('.item-content').empty();
 			ajax();
 		}
 	});
